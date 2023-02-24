3be32dd157a9c2e4f47788cea0814bd4d3f715cf
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-24 08:58:08 +0000

    feat: add layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import SingleProduct from './pages/product/SingleProduct';
+import Products from './pages/Category/Products';
+import Homepage from './pages/home/Homepage';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = ()=>{
+  return <div className="app">
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </div>
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <SingleProduct />,
+      },
+    ]
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
   return (
     <div className="App">
       <RouterProvider router={router} />
