1e522a5ed385680663e75321361bb4410052c89a
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-27 16:57:14 +0000

    feat: add data for the featured products

---
+++ src/components/featureProducts/productsData.ts
@@ -0,0 +1,39 @@
+export const productData = [
+  {
+    id: 1,
+    title: "Woman Wearing Black Leather Jacket and White Top",
+    price: 549,
+    oldPrice: 469,
+    isNew: true,
+    img: "https://images.pexels.com/photos/3062624/pexels-photo-3062624.jpeg",
+    img2: "https://images.pexels.com/photos/2794562/pexels-photo-2794562.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 2,
+    title: "Woman With Braided Hair Leaning on Wall",
+    price: 520,
+    oldPrice: 745,
+    isNew: true,
+    img: "https://images.pexels.com/photos/1958738/pexels-photo-1958738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img2: "https://images.pexels.com/photos/2693849/pexels-photo-2693849.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    title: "Woman Wearing Pink Overcoat and Black Inner Top",
+    price: 333,
+    oldPrice: 201,
+    isNew: false,
+    img: "https://images.pexels.com/photos/2043590/pexels-photo-2043590.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img2: "https://images.pexels.com/photos/3775001/pexels-photo-3775001.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 4,
+    title: "Woman Wearing Gingham Dress",
+    price: 500,
+    oldPrice: 409,
+    isNew: false,
+    img: "https://images.pexels.com/photos/859192/pexels-photo-859192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img2: "https://images.pexels.com/photos/859199/pexels-photo-859199.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  
+];
