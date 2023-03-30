34f4283c5857ee5e5f574fb2633e765b31d760c9
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-03-30 09:51:53 +0000

    refactor: add function to get the type of  products

--- src/components/featureProducts/FeatureProducts.tsx
+++ src/components/featureProducts/FeatureProducts.tsx
@@ -1,28 +1,42 @@
-import React from "react";
+/* eslint-disable no-template-curly-in-string */
+import React, { useEffect } from "react";
 import "./featureProducts.css";
 import Card from "../Card/Card";
-import { productData } from "./productsData";
+import { useFetch } from "../../hooks/useFetch";
+import { Spinner } from "flowbite-react";
+import { FeatureProductsProps, ProductProps } from "../../types/types";
 
-interface Props  {
-    type: string
-};
+const FeatureProducts = ({ type }: FeatureProductsProps) => {
+  const { productData, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
-const FeatureProducts = ({type}: Props) => {
-  return <div className="featuredProducts">
-    <div className="top">
-        <h1 className="text-xl font-bold">{type}</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1 className="text-xl font-bold">{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
           suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
           lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas.</p>
-    </div>
-    <div className="bottom">
-        {productData && productData.map((product:any)=>(
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom">
+        {error ? (
+          "Something went wrong!"
+        ) : loading ? (
+          <Spinner aria-label="Default status example" size="xl" />
+        ) : (
+          productData &&
+          productData.map((product: ProductProps) => (
             <Card product={product} key={product?.id} />
-        ))}
+          ))
+        )}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default FeatureProducts;
