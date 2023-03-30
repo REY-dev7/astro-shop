eff9bf5fb46ab474166164fa3abb509fda5b64fb
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-03-30 08:49:20 +0000

    feat: add a fetch function to get products

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,20 +1,35 @@
-import React from "react";
-import { productData } from "../featureProducts/productsData";
+import React, { useEffect } from "react";
 import Card from "../Card/Card";
-import "./list.css"
+import "./list.css";
+import { useFetch } from "../../hooks/useFetch";
+import { ProductProps } from "../../types/types";
+import { Spinner } from "flowbite-react";
 
 type ListProps = {
-    catId: number;
-    maxPrice: number;
-    sort: string | null;
+  catId: number;
+  maxPrice: number;
+  sort: string | null;
+  subCates: any[];
 };
 
-const List = ({catId, maxPrice, sort}: ListProps) => {
+const List = ({ catId, maxPrice, sort, subCates }: ListProps) => {
+  let { productData, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCates.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
+  
   return (
     <div className="list">
-      {productData?.map((product: any) => (
-        <Card product={product} key={product?.id} />
-      ))}
+      {loading ? (
+        <div className="flex justify-center w-full">
+          <Spinner aria-label="Default status example" size="xl" />
+        </div>
+      ) : (
+        productData?.map((product: ProductProps) => (
+          <Card product={product} key={product?.id} />
+        ))
+      )}
     </div>
   );
 };
