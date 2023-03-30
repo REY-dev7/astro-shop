2e4b779293c223236b49966859bd21ee3f90357b
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-03-30 09:48:45 +0000

    refactor: add function to get the products

--- src/pages/Category/Products.tsx
+++ src/pages/Category/Products.tsx
@@ -2,38 +2,65 @@ import React, { useState } from "react";
 import "./products.css";
 import List from "../../components/List/List";
 import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import { SubcategoryProps } from "../../types/types";
 
-type Props = {};
-
-const Products = (props: Props) => {
+const Products = () => {
   const params = useParams();
+  const catId = parseInt(params?.id as string);
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState<string | null>(null);
-  
-  
-  const catId = parseInt(params?.id as string);
-    
+  const [selectedSubCates, setSelectedSubCates] = useState<any[]>([]);
+
+  const { productData, loading, error } = useFetch(
+    `/sub-categories?[filters][categories][id][$eq]=${catId}`
+  );
+
+  const handleChangeSubCategories = (e: any) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCates(
+      isChecked
+        ? [...selectedSubCates, value]
+        : selectedSubCates.filter((item) => item !== value)
+    );
+  };
+
+  // console.log("selectedSubCates", selectedSubCates);
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h1>Product Categories</h1>
           <div className="inputItem">
-            <div className="">
-              <input type="checkbox" name="" id="1" value={1} />
-              <label htmlFor="1">shoes</label>
-            </div>
-            <div className="">
-              <input type="checkbox" name="" id="2" value={2} />
-              <label htmlFor="2">tshirt</label>
-            </div>
+            {productData?.map((item: SubcategoryProps) => (
+              <div
+                className=""
+                key={item?.id}
+                onChange={handleChangeSubCategories}
+              >
+                <input
+                  type="checkbox"
+                  name=""
+                  id={item?.id.toString()}
+                  value={item?.id}
+                />
+                <label htmlFor="1">{item?.attributes?.title}</label>
+              </div>
+            ))}
           </div>
         </div>
         <div className="filterItem">
           <h1>Filter by Price</h1>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={1000} onChange={(e:any)=>setMaxPrice(e.target.value)} />
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e: any) => setMaxPrice(e.target.value)}
+            />
             <span>{maxPrice}</span>
           </div>
         </div>
@@ -41,19 +68,40 @@ const Products = (props: Props) => {
           <h1>Sort by</h1>
           <div className="inputItem">
             <div className="">
-              <input type="radio" name="price" id="asc" value="asc" onChange={(e)=>setSort("asc")} />
+              <input
+                type="radio"
+                name="price"
+                id="asc"
+                value="asc"
+                onChange={(e) => setSort("asc")}
+              />
               <label htmlFor="asc">Price (Lowest first)</label>
             </div>
             <div className="">
-              <input type="radio" name="price" id="desc" value="desc" onChange={(e)=>setSort("desc")} />
+              <input
+                type="radio"
+                name="price"
+                id="desc"
+                value="desc"
+                onChange={(e) => setSort("desc")}
+              />
               <label htmlFor="desc">Price (Highest first)</label>
             </div>
           </div>
         </div>
       </div>
       <div className="right">
-        <img className="catImg" src="https://images.pexels.com/photos/953262/pexels-photo-953262.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/953262/pexels-photo-953262.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <List
+          catId={catId}
+          maxPrice={maxPrice}
+          sort={sort}
+          subCates={selectedSubCates}
+        />
       </div>
     </div>
   );
