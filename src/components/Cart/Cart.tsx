8045f04cb296327f5a1bc74e3c334436f40eba1e
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-03-30 09:05:43 +0000

    feat: add functions to calculate products and the payment system

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,13 +1,47 @@
 import React from "react";
-import { productData } from "../featureProducts/productsData";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import "./cart.css";
+import { useDispatch, useSelector } from "react-redux";
+import { IProduct, removeFromCart, resetCart } from "../../redux/cartReducer";
+import { RootState } from "../../redux/store";
+import { makeRequest } from "../../utils/makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 
 type CartProps = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const Cart = ({ setOpen }: CartProps) => {
+  const products = useSelector((state: RootState) => state.cart.products);
+  const getImage = process.env.REACT_APP_UPLOAD_URL!;
+  const dispatch = useDispatch();
+
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((product: IProduct) => {
+      total += product?.quantity * product?.price;
+    });
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_51MqwLaENIc8mlIZVqn796IdIltXEBzJpwKEicebouxdoM9w4OgHd9PcYpxanAfsstvwTRhfBifequxEcVDFnpfT800MiKpg0or"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe: any = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="cart shadow-2xl bg-slate-200">
       <div className="flex justify-between">
@@ -20,25 +54,38 @@ const Cart = ({ setOpen }: CartProps) => {
         </p>
       </div>
       <div className="h-40 md:h-[450px] overflow-auto ">
-        {productData?.map((product) => (
+        {products?.map((product: IProduct) => (
           <div className="item h-[120px] border-b-4" key={product?.id}>
-            <img src={product?.img} alt="" />
-            <div className="details">
+            <img src={getImage + product?.img} alt="" />
+            <div className="details w-full">
               <h1>{product?.title}</h1>
               <p>{product?.desc?.substring(0, 70)}</p>
-              <div className="price">1 x ${product?.price}</div>
+              <div className="price">
+                {product?.quantity} x ${product?.price}
+              </div>
             </div>
-            <DeleteOutlinedIcon className="delete" />
+            <DeleteOutlinedIcon
+              className="delete"
+              onClick={() => dispatch(removeFromCart(product?.id))}
+            />
           </div>
         ))}
       </div>
       <div className="total uppercase">
         <span>subtotal</span>
-        <span>$123</span>
+        <span>${totalPrice()}</span>
       </div>
       <div className="flex justify-between">
-      <button className="uppercase">proceed to checkout</button>
-      <span className="reset capitalize pt-3">reset cart</span></div>
+        <button className="uppercase" onClick={handlePayment}>
+          proceed to checkout
+        </button>
+        <span
+          className="reset capitalize pt-3"
+          onClick={() => dispatch(resetCart())}
+        >
+          reset cart
+        </span>
+      </div>
     </div>
   );
 };
