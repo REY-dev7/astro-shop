c820549ec038cdeca585bc6e4681f609c7e4f2f9
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-03-30 09:50:19 +0000

    feat: create base fetch for single product

---
+++ src/hooks/useFetchforSingleProduct.ts
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { ProductProps } from "../types/types";
+import { makeRequest } from "../utils/makeRequest";
+import { useParams } from "react-router-dom";
+
+export const useFetchforSingleProduct = () => {
+  const [singleProductData, setSingleProductData] = useState<ProductProps>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const productId = useParams().id;
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const res = await makeRequest.get(`/products/${productId}?populate=*`);
+        if (res.status === 200) {
+          setSingleProductData(res?.data?.data);
+        }
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [productId]);
+  return { singleProductData, loading, error };
+};
