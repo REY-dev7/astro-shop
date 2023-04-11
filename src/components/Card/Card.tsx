ded77e3e592363c5cc9762893a32f1e4b71134e0
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-11 08:36:32 +0000

    refactor: change the css style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./card.css";
-import {  ProductProps } from "../../types/types";
+import { ProductProps } from "../../types/types";
 
-const Card = ({product }:{product:ProductProps}) => {
+const Card = ({ product }: { product: ProductProps }) => {
   const getImage = process.env.REACT_APP_UPLOAD_URL;
-    
+
   return (
     <Link to={`/product/${product?.id}`} className="link">
-      <div className="card">
-        <div className="image">
+      <div className="card w-40 md:w-80 lg:w-[280px]">
+        <div className="image h-40 md:h-80 lg:h-[400px]">
           {product?.attributes?.isNew && <span>New Season</span>}
           <img
             src={getImage + product?.attributes?.img?.data?.attributes?.url}
             alt=""
             className="mainImg"
           />
-          <img src={getImage + product?.attributes?.img2?.data?.attributes?.url} alt="" className="secondImg" />
+          <img
+            src={getImage + product?.attributes?.img2?.data?.attributes?.url}
+            alt=""
+            className="secondImg"
+          />
         </div>
-        <h1>{product?.attributes?.title}</h1>
+        <h1 className="font-bold">{product?.attributes?.title}</h1>
         <div className="prices">
-          <h3>
-            ${ product?.attributes?.price + 20}
-          </h3>
+          <h3>${product?.attributes?.price + 20}</h3>
           <h3>${product?.attributes?.price}</h3>
         </div>
       </div>
