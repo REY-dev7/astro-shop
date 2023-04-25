1956764e00f54063ccfaefbb955d9cca280c6207
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-25 07:58:23 +0000

    feat: add another routes with their components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import SingleProduct from './pages/product/SingleProduct';
 import Products from './pages/Category/Products';
 import Homepage from './pages/home/Homepage';
-import NavbarComponent from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import About from './pages/aboutPage/About';
+import ContactPage from './pages/contactUs/ContactUs';
+import AccountPage from './pages/accounts/AccountPage';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import NavbarComponent from './components/Navbar/Navbar';
+import Logout from './components/logout/Logout';
+import ForgotPassword from './pages/accounts/forgetReset/ForgetPassword';
+
 
 const Layout = ()=>{
-  return <div className="app w-full">
+  return <div className="overflow-x-hidden">
     <NavbarComponent />
     <Outlet />
     <Footer />
@@ -30,14 +38,35 @@ const router = createBrowserRouter([
         path: "/product/:id",
         element: <SingleProduct />,
       },
+      {
+        path: "/about-page",
+        element: <About />,
+      },
+      {
+        path: "/contact-page",
+        element: <ContactPage />,
+      },
     ]
+  },
+  {
+    path: "/login",
+    element: <AccountPage />,
+  },
+  {
+    path: "/logout",
+    element: <Logout />,
+  },
+  {
+    path: "/reset-password",
+    element: <ForgotPassword />,
   },
 ]);
 
 function App() {
   return (
-    <div className="App">
+    <div className="App overflow-x-hidden">
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
