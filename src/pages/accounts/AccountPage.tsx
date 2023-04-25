94b0a9c58e8068d6a6cf8faf2da4b4eb59f4822f
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-25 08:06:56 +0000

    feat: add account page component and its css file

---
+++ src/pages/accounts/AccountPage.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { LoginForm } from "../../components/login/Login";
+import "./account.css";
+import SignUp from "../../components/signup/SignUp";
+
+type Props = {};
+
+const AccountPage = (props: Props) => {
+  const [signUp, setSignUp] = useState(false);
+  return (
+    <div className="homePage">
+      <div className="sign-in">
+        <div className="bg-white fade-bottom h-[100vh] w-full flex justify-center">
+          <div className="main flex flex-col place-items-center border-b-2 border-black/20 ">
+            <h1 className="text-2xl font-serif">
+              <a href="/">ASTRO SHOP</a>
+            </h1>
+            <h1 className="text-2xl font-mono font-extrabold">
+              {signUp ? "Sign Up" : "Sign In"}
+            </h1>
+            <p className=" text-gray-500">
+              Enter your credentials to {signUp ? "have an account" : "your account"}
+            </p>
+            {signUp ? (
+              <SignUp setSignUp={setSignUp} />
+            ) : (
+              <LoginForm setSignUp={setSignUp} />
+            )}
+            <div className="flex items-center justify-between w-full px-4 py-4">
+              <p className="hover:text-red-600 cursor-pointer">Terms of service</p>
+              <p className="hover:text-red-600 cursor-pointer">Privacy policy</p>
+              <p className="hover:text-red-600 cursor-pointer">Contact support</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountPage;
