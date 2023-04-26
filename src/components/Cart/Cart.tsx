0660d3ed9f5315c0454da584d342258cbeedab82
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-26 15:25:23 +0000

    feat: fetch the user data from localStorage to determine the payment

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -6,16 +6,18 @@ import { IProduct, removeFromCart, resetCart } from "../../redux/cartReducer";
 import { RootState } from "../../redux/store";
 import { makeRequest } from "../../utils/makeRequest";
 import { loadStripe } from "@stripe/stripe-js";
+import { useNavigate } from "react-router-dom";
 
 type CartProps = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  open: boolean
+  open: boolean;
 };
 
 const Cart = ({ setOpen, open }: CartProps) => {
   const products = useSelector((state: RootState) => state.cart?.products);
   const getImage = process.env.REACT_APP_UPLOAD_URL!;
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const totalPrice = () => {
     let total = 0;
@@ -24,22 +26,27 @@ const Cart = ({ setOpen, open }: CartProps) => {
     });
     return total.toFixed(2);
   };
+  const userData = localStorage.getItem("JWT");
 
   const stripePromise = loadStripe(
     "pk_test_51MqwLaENIc8mlIZVqn796IdIltXEBzJpwKEicebouxdoM9w4OgHd9PcYpxanAfsstvwTRhfBifequxEcVDFnpfT800MiKpg0or"
   );
 
   const handlePayment = async () => {
-    try {
-      const stripe = await stripePromise;
-      const res = await makeRequest.post("/orders", {
-        products,
-      });
-      await stripe?.redirectToCheckout({
-        sessionId: res?.data?.stripeSession?.id,
-      });
-    } catch (err) {
-      console.log(err);
+    if (userData) {
+      try {
+        const stripe = await stripePromise;
+        const res = await makeRequest.post("/orders", {
+          products,
+        });
+        await stripe?.redirectToCheckout({
+          sessionId: res?.data?.stripeSession?.id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      navigate("/login");
     }
   };
 
@@ -57,7 +64,7 @@ const Cart = ({ setOpen, open }: CartProps) => {
       <div className="h-[50vh] md:h-[450px] overflow-auto ">
         {products?.map((product: IProduct) => (
           <div className="item h-[120px] border-b-4" key={product?.id}>
-            <img src={getImage + product?.img} alt=""/>
+            <img src={getImage + product?.img} alt="" />
             <div className="details w-full ">
               <h2 className="font-bold text-xl text-black">{product?.title}</h2>
               <p>{product?.desc?.substring(0, 70)}</p>
@@ -76,9 +83,12 @@ const Cart = ({ setOpen, open }: CartProps) => {
         <span>subtotal</span>
         <span>${totalPrice()}</span>
       </div>
-      <div className="flex justify-between" >
-        <button className="uppercase bg-[#5b96f5] hover:bg-[#2879fe] px-4 py-2 rounded-lg text-white font-bold" onClick={handlePayment}>
-          proceed to checkout 
+      <div className="flex justify-between">
+        <button
+          className="uppercase bg-[#5b96f5] hover:bg-[#2879fe] px-4 py-2 rounded-lg text-white font-bold"
+          onClick={handlePayment}
+        >
+          proceed to checkout
         </button>
         <span
           className="reset capitalize pt-3"
