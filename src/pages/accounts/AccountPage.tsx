6007679ed06478d4a5619f39515b8a004f0bc833
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-26 15:34:34 +0000

    feat: pass in props from the useState

--- src/pages/accounts/AccountPage.tsx
+++ src/pages/accounts/AccountPage.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { LoginForm } from "../../components/login/Login";
 import "./account.css";
 import SignUp from "../../components/signup/SignUp";
+import { SignUpErrorState } from "../../types/types";
 
 type Props = {};
 
 const AccountPage = (props: Props) => {
   const [signUp, setSignUp] = useState(false);
+  const [errors, setErrors] = useState({});
   return (
     <div className="homePage">
       <div className="sign-in">
@@ -22,9 +24,9 @@ const AccountPage = (props: Props) => {
               Enter your credentials to {signUp ? "have an account" : "your account"}
             </p>
             {signUp ? (
-              <SignUp setSignUp={setSignUp} />
+              <SignUp setSignUp={setSignUp} setErrors={setErrors} errors={errors} />
             ) : (
-              <LoginForm setSignUp={setSignUp} />
+              <LoginForm setSignUp={setSignUp} setErrors={setErrors} errors={errors} />
             )}
             <div className="flex items-center justify-between w-full px-4 py-4">
               <p className="hover:text-red-600 cursor-pointer">Terms of service</p>
