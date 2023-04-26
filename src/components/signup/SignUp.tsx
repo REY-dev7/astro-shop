45d7f7ffdc4ed1b8117fe1bbb08581322090e62f
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-04-26 15:33:33 +0000

    feat: add the validation function

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -4,18 +4,15 @@ import { MdOutlineMail, MdLockOutline } from "react-icons/md";
 import { RxEyeClosed } from "react-icons/rx";
 import { AiOutlineUser } from "react-icons/ai";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
-type SignUpProps = {
-  setSignUp: React.Dispatch<React.SetStateAction<boolean>>;
-};
+import { toast } from "react-toastify";
+import { validateForm } from "../../utils/functions";
+import { SignUpProps } from "../../types/types";
 
 const initialUser = { username: "", email: "", password: "" };
 
-const SignUp = ({ setSignUp }: SignUpProps) => {
+const SignUp = ({ setSignUp, errors, setErrors }: SignUpProps) => {
   const [user, setUser] = useState(initialUser);
   const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate();
 
   const handleUserChange = ({ target }: any) => {
     const { name, value } = target;
@@ -24,92 +21,123 @@ const SignUp = ({ setSignUp }: SignUpProps) => {
       [name]: value,
     }));
   };
-  const handleSubmit = async () => {
-    const url = "http://localhost:1337/api/auth/local/register";
-    try {
-      if (user.username && user.email && user.password) {
-        const res = await axios.post(url, user);
-        if (res) {
-          setUser(initialUser);
-          // navigate("/login");
-          setSignUp(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const url = `${process.env.REACT_APP_UPLOAD_URL}/api/auth/local/register`;
+    const validationErrors = validateForm(user);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      try {
+        const { data } = await axios.post(url, user);
+        if (!data.ok) {
+          setErrors(data.errors);
+        } else {
+          setUser({
+            username: user.username,
+            email: user.email,
+            password: user.password,
+          });
+          setErrors({});
+          toast.success("Sign up successful!", {
+            hideProgressBar: true,
+          });
         }
+        setSignUp(false);
+        // if (user.username && user.email && user.password) {
+        //   const { data } = await axios.post(url, user);
+        //   if (data) {
+        //     setUser(initialUser);
+        //     toast.success("Sign up successfully!", {
+        //       hideProgressBar: true,
+        //     });
+        // setSignUp(false);
+        //   }
+        // }
+      } catch (error) {
+        toast.error("Username and Email have been used", {
+          hideProgressBar: true,
+          theme: "colored",
+        });
       }
-    } catch (error) {
-      console.log(error);
-      // toast.error(error.message, {
-      // position: "top-right",
-      // autoClose: 5000,
-      // hideProgressBar: true,
-      // closeOnClick: true,
-      // pauseOnHover: true,
-      // draggable: true,
-      // progress: undefined,
-      // theme: "light",
-      // });
     }
   };
 
   return (
-    <div className="w-full px-10">
-      <div className="input-div relative w-full my-5">
-        <div className="flex absolute inset-y-0 left-0 pt-3 pl-3 pointer-events-none">
-          <AiOutlineUser />
+    <form onSubmit={handleSubmit} className="w-full px-10">
+      <div className="my-5">
+        <div className="input-div relative w-full ">
+          <div className="flex absolute inset-y-0 left-0 pt-3 pl-3 pointer-events-none">
+            <AiOutlineUser />
+          </div>
+          <input
+            type="text"
+            id="name"
+            name="username"
+            className="border border-[#d8dae5] hover:border-[#8F95B2] txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 "
+            placeholder="Full Name"
+            value={user?.username}
+            onChange={handleUserChange}
+          />
         </div>
-        <input
-          type="text"
-          id="name"
-          name="username"
-          className="border border-[#d8dae5] hover:border-[#8F95B2] focus:outline-[#88eb74] focus:ring-2 focus:ring-[#88eb74] txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#F2BEAB] dark:focus:border[#d8dae5]"
-          placeholder="Full Name"
-          value={user?.username}
-          onChange={handleUserChange}
-        />
+        {errors.username && (
+          <p className="text-red-600 text-xs">{errors.username}</p>
+        )}
       </div>
-      <div className="input-div relative w-full my-5">
-        <div className="flex absolute inset-y-0 left-0 pt-3 pl-3 pointer-events-none">
-          <MdOutlineMail />
+      <div className="my-5">
+        <div className="input-div relative w-full ">
+          <div className="flex absolute inset-y-0 left-0 pt-3 pl-3 pointer-events-none">
+            <MdOutlineMail />
+          </div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="border border-[#d8dae5] hover:border-[#8F95B2]  txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 "
+            placeholder="Email"
+            value={user.email}
+            onChange={handleUserChange}
+          />
         </div>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="border border-[#d8dae5] hover:border-[#8F95B2] focus:outline-[#88eb74] focus:ring-2 focus:ring-[#88eb74] txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#F2BEAB] dark:focus:border[#d8dae5]"
-          placeholder="Email"
-          value={user.email}
-          onChange={handleUserChange}
-        />
+        {errors.email && <p className="text-red-600 text-sm">{errors.email}</p>}
       </div>
-      <div className="input-div relative w-full mb-8">
-        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-          <MdLockOutline />
+      <div className="mb-8">
+        <div className="input-div relative w-full ">
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <MdLockOutline />
+          </div>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            data-cy="password"
+            name="password"
+            className="border border-[#d8dae5] hover:border-[#8F95B2]  txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 "
+            placeholder="Password"
+            value={user.password}
+            onChange={handleUserChange}
+          />
+          <span
+            className="flex absolute inset-y-0 right-5 items-center"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <BiShow /> : <RxEyeClosed />}
+          </span>
         </div>
-        <input
-          type={showPassword ? "text" : "password"}
-          id="password"
-          data-cy="password"
-          name="password"
-          className="border border-[#d8dae5] hover:border-[#8F95B2] focus:outline-[#88eb74] focus:ring-[#88eb74] focus:ring txt-field-style peer text-gray-900 text-sm block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#F2BEAB] dark:focus:border[#d8dae5]"
-          placeholder="Password"
-          value={user.password}
-          onChange={handleUserChange}
-        />
-        <button
-          className="flex absolute inset-y-0 right-5 items-center"
-          onClick={() => setShowPassword(!showPassword)}
-        >
-          {showPassword ? <BiShow /> : <RxEyeClosed />}
-        </button>
+        {errors.password && (
+          <p className="text-red-600 text-sm">{errors.password}</p>
+        )}
       </div>
       <div className="flex items-center justify-between w-full">
         <button
-          onClick={handleSubmit}
+          type="submit"
+          // onClick={handleSubmit}
           className="text-white btn-shadow bg-[#667ee9] hover:bg-[#1d27b5] w-full rounded-lg px-12 py-2"
         >
           Sign Up
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
